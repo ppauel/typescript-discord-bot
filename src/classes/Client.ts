ade47e469f4ebe0bd6dfa0e13a0f96ed6a55bec3
John William Bowen <johnwb1776@gmail.com>
2023-03-20 22:17:30 -0400

    formanting change

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -219,35 +219,12 @@ function isErrnoException(error: unknown): error is NodeJS.ErrnoException {
 }
 
 declare module 'discord.js' {
-    interface BaseInteraction {
-        client: ExtendedClient
-    }
-
-    interface Component {
-        client: ExtendedClient
-    }
-
-    interface Message {
-        client: ExtendedClient
-    }
-
-    interface BaseChannel {
-        client: ExtendedClient
-    }
-
-    interface Role {
-		client: ExtendedClient
-	}
-
-    interface Guild {
-		client: ExtendedClient
-	}
-
-	interface User {
-		client: ExtendedClient
-	}
-
-	interface GuildMember {
-		client: ExtendedClient
-	}
+    interface BaseInteraction { client: ExtendedClient}
+    interface Component { client: ExtendedClient }
+    interface Message { client: ExtendedClient }
+    interface BaseChannel { client: ExtendedClient }
+    interface Role { client: ExtendedClient }
+    interface Guild { client: ExtendedClient }
+	interface User { client: ExtendedClient }
+	interface GuildMember { client: ExtendedClient }
 }
