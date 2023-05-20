ba2aae7af0669332972b0d6d07f1acb7932e1ca5
John William Bowen <johnwb1776@gmail.com>
2023-05-20 12:50:42 -0400

    removed deployment from ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,7 +5,5 @@ export default new Event()
     .setName(Events.ClientReady)
     .setOnce(true)
     .setExecute(async (client:Client) => {
-        // Skip if no-deployment flag is set, else deploys commands
-        if (!process.argv.includes('--no-deployment')) await client.deploy();
         console.log(`\nReady! Logged in as ${client.user?.tag} (${client.user?.id})\n`);
     });
