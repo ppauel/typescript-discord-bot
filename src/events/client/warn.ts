e6a4c8e2a7736f9f6af0b7c7408eeee41e933f34
John William Bowen <johnwb1776@gmail.com>
2024-04-25 11:01:42 -0400

    style fixes

--- src/events/client/warn.ts
+++ src/events/client/warn.ts
@@ -2,6 +2,6 @@ import { Events } from 'discord.js';
 import { Event } from '../../Classes/index.js';
 
 export default new Event({
-	name: Events.Warn,
-	execute: async (info: string) => console.warn(info)
+    name: Events.Warn,
+    execute: async (info: string) => console.warn(info)
 });
