61732fcf21fff96b5896b9ac2bd335565bccb2ff
pauel <hello@pauel.eu>
2024-09-19 13:27:13 +0200

    Fix command interface

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,26 +1,56 @@
-import ExtendedClient from '../classes/Client';
-import { ChatInputCommandInteraction, CommandInteraction, ContextMenuCommandBuilder, ContextMenuCommandInteraction, MessageContextMenuCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder, UserContextMenuCommandInteraction } from 'discord.js';
+import {
+    ChatInputCommandInteraction,
+    CommandInteraction,
+    ContextMenuCommandBuilder,
+    ContextMenuCommandInteraction,
+    MessageContextMenuCommandInteraction,
+    SlashCommandBuilder,
+    SlashCommandOptionsOnlyBuilder,
+    SlashCommandSubcommandsOnlyBuilder,
+    UserContextMenuCommandInteraction,
+} from "discord.js";
+import ExtendedClient from "../classes/Client";
+
+type AnySlashCommandBuilder =
+    | SlashCommandBuilder
+    | SlashCommandSubcommandsOnlyBuilder
+    | SlashCommandOptionsOnlyBuilder;
 
 export interface Command {
-    options: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'> | ContextMenuCommandBuilder,
-    global: boolean,
-    execute(client: ExtendedClient, interaction: CommandInteraction): Promise<void>
+    options: AnySlashCommandBuilder | ContextMenuCommandBuilder;
+    global: boolean;
+    execute(
+        client: ExtendedClient,
+        interaction: CommandInteraction,
+    ): Promise<void>;
 }
 
 export interface ChatInputCommand extends Command {
-    options: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>
-    execute(client: ExtendedClient, interaction: ChatInputCommandInteraction): Promise<void>
+    options: AnySlashCommandBuilder;
+    execute(
+        client: ExtendedClient,
+        interaction: ChatInputCommandInteraction,
+    ): Promise<void>;
 }
 
 export interface ContextMenu extends Command {
-    options: ContextMenuCommandBuilder
-    execute(client: ExtendedClient, interaction: ContextMenuCommandInteraction): Promise<void>
+    options: ContextMenuCommandBuilder;
+    execute(
+        client: ExtendedClient,
+        interaction: ContextMenuCommandInteraction,
+    ): Promise<void>;
 }
 
 export interface UserContextMenu extends ContextMenu {
-    execute(client: ExtendedClient, interaction: UserContextMenuCommandInteraction): Promise<void>
+    execute(
+        client: ExtendedClient,
+        interaction: UserContextMenuCommandInteraction,
+    ): Promise<void>;
 }
 
 export interface MessageContextMenu extends ContextMenu {
-    execute(client: ExtendedClient, interaction: MessageContextMenuCommandInteraction): Promise<void>
+    execute(
+        client: ExtendedClient,
+        interaction: MessageContextMenuCommandInteraction,
+    ): Promise<void>;
 }
